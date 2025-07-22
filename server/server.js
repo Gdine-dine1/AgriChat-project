cd404ebf7a3c9a2dc96f0c3be79391c545bdf620
gdine-dine1 <ngungegeradine7@gmail.com>
2025-07-22 17:35:31 +0300

    trial 6

--- server/server.js
+++ server/server.js
@@ -36,7 +36,15 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: true,
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 // Handle preflight requests for all routes
