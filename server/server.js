450e3a0f7325adfd5a9b2daf6d35f8f25d9f6786
gdine-dine1 <ngungegeradine7@gmail.com>
2025-07-22 07:50:41 +0300

    cors fixed

--- server/server.js
+++ server/server.js
@@ -30,7 +30,11 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://agri-chat-project.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
